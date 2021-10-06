3bd3e94148a46a9ab968a5030e782896154834c8
mokumoku-chan <maho.kumo1997@gmail.com>
2021-10-06 14:20:46 +0000

    [Add]indexページレイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,15 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
+
   end
 
   def show
   end
 
   def new
-    @book = Book.new
+
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to => 'homes#top'
-  resources :books
+
+  scope(path_names: {new: ''}) do
+    resources :books
+  end
 
 
 end
